04fc2e546742d70d7fdc63553ed127281a95cc7c
Adilson-Jr11 <160561320+Adilson-Jr11@users.noreply.github.com>
2024-08-27 21:55:04 -0300

    Add files via upload

---
+++ CódigosPython/exer01.py
@@ -0,0 +1,8 @@
+tempF=input("Digite a temperatura em Fahrenheit:")
+
+tempF=float(tempF) #converte o texto 
+
+tempC = 5/9 * (tempF - 32) #calculo para conversão em Celcius
+
+#mostrando o resultado
+print("A temperatura em graus Celscius é",tempC)
---
+++ CódigosPython/exer02.py
@@ -0,0 +1,9 @@
+base=input("digite o valor da base:")
+altura=input("digite o valor da altura:")
+
+base=int(base)
+altura=int(altura)
+
+triangulo=(base * altura)/2
+
+print("A area do Triangulo é ", triangulo)
